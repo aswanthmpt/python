647738b958a64fd0c624fb7f2762fe5ae60f6675
aswanthmpt <aswanthmpt@gmail.com>
2023-12-05 11:17:58 +0530

    new

--- tkinder/message.py
+++ tkinder/message.py
@@ -17,22 +17,29 @@
 # root.mainloop()
 
 from tkinter import *
-# from tkinter import messagebox
-root=Tk()
-l1=Label(root,text="enter first number")
-e1=Entry(root)
+
+rot=Tk()
+frame=Frame(rot)
+frame.pack()
+l1=Label(frame,text="enter first number")
+e1=Entry(frame)
 l1.pack()
 e1.pack()
-l2=Label(root,text="enter second number")
-e2=Entry(root)
+l2=Label(frame,text="enter second number")
+e2=Entry(frame)
 l2.pack()
 e2.pack()
-# def asd():
-#    messagebox.askquestion("Delete","are you sure to quite")
-bt2=Button(root,text="add")
-# bti=Button(root,text="exit")
-# bt2.grid(row=0,column=0)
-bt2.pack()
-# bti.grid(row=0,column=1)
 
-root.mainloop()
+fr=Frame(rot)
+fr.pack()
+bt=Button(fr,text="add")
+bt.grid(row=0,column=0)
+btt=Button(fr,text="exit",command=fr.quit)
+btt.grid(row=0,column=1)
+
+frq=Frame(rot)
+frq.pack()
+l2=Label(frq,text="result")
+l2.pack()
+
+rot.mainloop()
