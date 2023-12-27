8e2b1149dec17589d43fe23ac81f28f26b599d00
aswanthmpt <aswanthmpt@gmail.com>
2023-12-27 09:00:41 +0530

    new

---
+++ xam/new.py
@@ -0,0 +1,60 @@
+###1
+
+# ls=[1,8,4,5,6,9]
+# lss=[0]
+
+# def asd(a):
+#     lss[0]
+    
+#     for i in ls:
+#         if i%2==0:
+#             lss.append(i)
+#     return lss
+# x=asd(ls)
+# print(x)
+
+###2
+###3
+###4
+c=0
+class dep:
+   
+   def deposit(self):
+     
+      a=int(input("enter the deposit amount:"))
+      c=c+a
+class wid:
+   def widraw(self):
+      b=int(input("enter the widrawal amount:"))
+      c-b
+class bal:
+   def balance(self):
+      print(c)
+      
+d=dep()
+d.deposit()
+      
+   
+###5
+
+# lss=[1,2,3,4,5,6,7,8,9,10]
+# ls=[i**2 for i in lss]
+# print(lss)
+# print(ls)
+
+    
+##6
+# a=int(input("enter a number"))
+# def fact(n):
+#    if n == 1:
+#        return n
+#    else:
+#        return n*fact(n-1)
+# num=a
+# if num == 0:
+#    print("The factorial of 0 is 1")
+# else:
+#    print("The factorial of", num, "is", fact(num))
+
+###7
+# dict={'aa':10,'bb':30,'cc':20,'dd':50,'dd':60,}
